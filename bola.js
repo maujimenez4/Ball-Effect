fd0c1e255d951480062c70dbaa9f3e89bfce560c
Mauricio Jimenez <158120415+maujimenez4@users.noreply.github.com>
2024-11-08 11:19:40 -0600

    feat: add move function with x and y direction parameters.js
    Implemented a new `move` function that accepts `x` and `y` direction parameters to control the movement of objects. This update allows for more flexible positioning based on directional input.

--- bola.js
+++ bola.js
@@ -4,6 +4,10 @@ class Bola{
         this.x = x
         this.y = y
         this.radio = 10
+        this.dirY = (Math.random() * 2) - 1 
+        this.dirX = (Math.random() * 2) - 1
+        this.velocidad = 200
+        
 
     }
 
@@ -12,6 +16,13 @@ class Bola{
         ctx.arc(this.x, this.y, this.radio, 0, Math.PI*2)
         ctx.fill()
         ctx.closePath()
+    }
+
+    mover(){
+        this.x += this.dirX * this.velocidad
+        this.y += this.dirY * this.velocidad
+
+
     }
 
 }
