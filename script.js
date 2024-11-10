61c6728a229d7dd0ea21dd514d4d4db693ae422f
Mauricio Jimenez <158120415+maujimenez4@users.noreply.github.com>
2024-11-09 21:05:37 -0600

    feat: increase ball count for visual quality and add trail effect to connect balls
    Increased the number of balls to enhance visual quality and added a trail effect that connects all balls, creating a cohesive animation. This update improves the overall appearance and interactivity.

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ canvas.height = innerHeight
 
 let bolas = []
 
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 30; i++) {
     bolas.push(new Bola (canvas.width / 2, canvas.height / 2))
     
 }
 
 function animar(){
+
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+
     bolas.forEach(bola=>{
+
+        bolas.forEach(bola2 => {
+            ctx.beginPath()
+            ctx.moveTo(bola.x, bola.y)
+            ctx. lineTo(bola2.x, bola2.y)
+            ctx.stroke()
+            ctx.closePath()
+        })
+
         bola.dibujar()
         bola.mover()
     })
